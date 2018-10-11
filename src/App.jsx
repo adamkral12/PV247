833722be0ad869dfd2a9af636e710ea6f59c0a0b
Adam Král <adam.kral@kiwi.com>
2018-10-11 10:07:31 +0200

    fix lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import ChannelWrapper from './Channels/ChannelWrapper';
 import data from './tests/mockData.json';
-import Screen from "./Screen/Screen"; //mock data
+import Screen from "./Screen/Screen"; // mock data
 
 class App extends Component {
     onChannelClick = (channelId) => {
-        console.log("Event bubbled into top component, channel id = " + channelId);
+      console.log("Event bubbled into top component, channel id = " + channelId);
     };
 
-  render() {
-    return (
+    render() {
+      return (
       <div className="App">
           <div className="col-xs-3">
           <ChannelWrapper
-              channels={data.channels}
-              onChannelClick={(channelId) => this.onChannelClick(channelId)}
+            channels={data.channels}
+            onChannelClick={(channelId) => this.onChannelClick(channelId)}
           />
           </div>
           <div className="col-xs-9">
             <Screen/>
           </div>
       </div>
-    );
-  }
+      );
+    }
 }
 
 export default App;
