9f407031f2099a0f4d694b64ae6883809a6be731
Jozef Pastucha <xpastuch@fi.muni.cz>
2018-10-12 12:31:39 +0200

    Channel Navbar, pullRight doesn't work properly without fluid property on Navbar

--- src/Screen/Body/MessageBoard.js
+++ src/Screen/Body/MessageBoard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Post from "./Post";
 import MessageForm from "./MessageForm";
+import ChannelNavbar from "./ChannelNavbar";
 
 export default class MessageBoard extends Component {
     render() {
         return (
             <div>
+                <ChannelNavbar/>
                 <Post/>
                 <Post/>
                 <Post/>
