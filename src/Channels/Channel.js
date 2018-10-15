a98d1bfb0a913b7280dd0e0da70c7629443782ec
Adam Král <adam.kral@kiwi.com>
2018-10-15 14:15:34 +0200

    delete channel modal

--- src/Channels/Channel.js
+++ src/Channels/Channel.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Button, Glyphicon } from 'react-bootstrap';
+import DeleteChannelModal from "./DeleteChannelModal";
 
 export default class Channel extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDeleteChannelModal: false
+        };
+    }
 
     // todo: this change will be propagated and change the main screen
     onclick = () => {
         console.log("on click in channel, calling props onClick");
         this.props.onClick(this.props.id);
+    };
+
+    showDeleteChannelModal = () => {
+        this.setState({ showDeleteChannelModal: true })
+    };
+
+    hideDeleteChannelModal = () => {
+        this.setState({ showDeleteChannelModal: false })
+    };
+
+    deleteChannel = () => {
+        this.setState({ showDeleteChannelModal: false })
+        // TODO:  delete channel (this.props.id)
     };
 
     render() {
@@ -17,6 +39,18 @@ export default class Channel extends Component {
                 >
                     {this.props.name}
                 </div>
+                <Button bsStyle="danger"
+                        onClick={this.showDeleteChannelModal}
+                >
+                    <Glyphicon glyph="remove"/>
+                </Button>
+                <DeleteChannelModal
+                    channelName={this.props.name}
+                    channelId={this.props.id}
+                    show={this.state.showDeleteChannelModal}
+                    onClose={this.hideDeleteChannelModal}
+                    onDelete={this.deleteChannel}
+                />
             </div>
         );
     }
---
+++ src/Channels/DeleteChannelModal.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+export default class DeleteChannelModal extends Component {
+    handleClose = () => {
+        this.setState({ show: false });
+    };
+
+    handleShow = () => {
+        this.setState({ show: true });
+    };
+
+    render() {
+        return (
+            <div>
+                <Modal show={this.props.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Delete channel {this.props.channelName}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Are you sure you want to delete this channel?
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button bsStyle="danger" onClick={this.props.onDelete}>Delete</Button>
+                        <Button onClick={this.props.onClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+DeleteChannelModal.propTypes = {
+    channelName: PropTypes.string.isRequired,
+    channelId: PropTypes.string.isRequired,
+    show: PropTypes.bool.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired
+};
