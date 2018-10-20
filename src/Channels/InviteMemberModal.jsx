c6192298ee7eef9c9aa76b6db008568d26a317ca
Jozef Pastucha <xpastuch@fi.muni.cz>
2018-10-20 02:09:05 +0200

    InviteMember

--- src/Channels/InviteMemberModal.jsx
+++ src/Channels/InviteMemberModal.jsx
@@ -6,13 +6,55 @@ import {
 
 import PropTypes from 'prop-types';
 import Member from "./Member";
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+
+
+import BootstrapTable from 'react-bootstrap-table-next';
+
 export default class InviteMemberModal extends Component {
     constructor(props) {
         super(props);
     }
 
+    nameFormatter(cell, row) {
+        return (<div className="text-center" align="middle">{cell}</div>)
+    }/*
+    imageFormatter(cell, row) {
+        return (<img style={{width:50}} src={cell}/>)
+    }*/
+    renderButtons(cell, row) {
+        return (
+            <span>
+        <Button
+            className="btn btn btn-primary btn-sm"
+            type="button"
+            data-toggle="tooltip"
+            recordable_id={cell}
+        >
+          Invite
+        </Button>
+      </span>
+        );
+    }
 
     render() {
+        const columns = [/*{
+            dataField: "customData.profilePicture",
+            formatter: this.imageFormatter
+        },*/
+            {
+                text: "", //"name",
+                dataField: 'customData.displayName',
+                //formatter:this.nameFormatter,
+                filter: textFilter() // apply text filter
+            },
+            {
+                dataField: 'email',
+                formatter: this.renderButtons
+            },
+        ]
+
         return (
             <div>
                 <Modal show={this.props.show} onHide={this.props.onClose}>
@@ -20,12 +62,17 @@ export default class InviteMemberModal extends Component {
                         <Modal.Title>Edit channel name</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
+                        <BootstrapTable
+                            headerClasses="show-hidden"
+                            keyField='email'
+                            data = {this.props.users}
+                            bordered={false}
+                            columns={columns}
+                            pagination={ paginationFactory() }
+                            filter={ filterFactory() }
+                        />
 
-                            {this.props.users.map((user) => {
-                                return (
-                                        <Member user = {user}/>
-                                    );
-                            })}
+
                     </Modal.Body>
                     <Modal.Footer>
                         <Button onClick={this.props.onClose}>Close</Button>
