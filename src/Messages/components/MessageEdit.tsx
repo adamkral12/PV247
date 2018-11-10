bbc9cbc14e5f0c4f57fc4caae2df8bb0fef63f6c
Adam Král <adam.kral@kiwi.com>
2018-11-11 00:24:22 +0100

    use react-bootstrap components

--- src/Messages/components/MessageEdit.tsx
+++ src/Messages/components/MessageEdit.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {IMessage} from '../model/IMessage';
+import {Form, FormControl, Button} from 'react-bootstrap';
 
 interface IProps {
     readonly message: IMessage;
@@ -33,15 +34,16 @@ export class MessageEdit extends React.PureComponent<IProps, IState> {
 
     render() {
         return (
-            <form onSubmit={this.onSave}>
-                <input
+            <Form onSubmit={this.onSave}>
+                <FormControl
+                    type="text"
                     value={this.state.value}
                     onChange={this.onValueChanged}
                     className="form-control"
                 />
-                <button type="submit" className="btn btn-primary">Save</button>
-                <button type="button" className="btn btn-default" onClick={this.props.onCancel}>Cancel</button>
-            </form>
+                <Button type="submit" bsStyle="primary">Save</Button>
+                <Button type="button" bsStyle="default" onClick={this.props.onCancel}>Cancel</Button>
+            </Form>
         );
     }
 }
