f754cea8aef1e5e2e9b6331277c97bea12daff24
Adam Král <adam.kral@kiwi.com>
2018-11-24 10:24:53 +0100

    remove redundant payload

--- src/Users/actions/actionCreators.ts
+++ src/Users/actions/actionCreators.ts
@@ -2,16 +2,10 @@ import {USER_APP_HIDE_EDIT_USER_MODAL, USER_APP_SHOW_EDIT_USER_MODAL, USER_APP_E
 
 export const showEditUser = (): Action => ({
     type: USER_APP_SHOW_EDIT_USER_MODAL,
-    payload: {
-        show: true,
-    }
 });
 
 export const hideEditUser = (): Action => ({
     type: USER_APP_HIDE_EDIT_USER_MODAL,
-    payload: {
-        show: false,
-    }
 });
 
 export const editUser = (email: string, profilePicture: string, displayName: string): Action => ({
