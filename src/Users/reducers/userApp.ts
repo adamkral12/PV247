c22f8162af7ef52fe578ca844e84e8a49f232f6c
Adam Král <adam.kral@kiwi.com>
2018-11-25 22:12:16 +0100

    fix user app styles

--- src/Users/reducers/userApp.ts
+++ src/Users/reducers/userApp.ts
@@ -1,12 +1,8 @@
 import {IUserApp} from '../model/IUserApp';
 import {showEditUserModal} from './showEditUserModal';
 import {users} from './users';
-import {showRegistrationModal} from '../../Authentication/reducers/showRegistrationlModal';
-import {showLoginModal} from '../../Authentication/reducers/showLoginModal';
 
 export const userApp = (prevState = {} as IUserApp, action: Action): IUserApp => ({
     users: users(prevState.users, action),
     showEditUserModal: showEditUserModal(prevState.showEditUserModal, action),
-    showLoginModal: showLoginModal(prevState.showLoginModal, action),
-    showRegistrationModal: showRegistrationModal(prevState.showRegistrationModal, action)
 });
