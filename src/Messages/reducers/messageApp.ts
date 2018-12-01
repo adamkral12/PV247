3aee2ab9d4233c03d0903f90770ee3ba825149ef
Jozef Pastucha <xpastuch@fi.muni.cz>
2018-12-01 13:50:56 +0100

    messages - loading for crud operations

--- src/Messages/reducers/messageApp.ts
+++ src/Messages/reducers/messageApp.ts
@@ -3,6 +3,7 @@ import {messages} from './messages';
 import {editedMessageId} from './editedMessageId';
 import {loadMessagesRespone} from './loadMessagesRespone';
 import {crudResponse} from './crudResponse';
+import {isLoading} from './isLoading';
 /*
 import {isLoading} from '../../Channels/reducers/isLoading';
 import {showMessages} from './showMessages';*/
@@ -10,8 +11,8 @@ import {showMessages} from './showMessages';*/
 export const messageApp = (prevState = {} as IMessageApp, action: Action): IMessageApp => ({
     messages: messages(prevState.messages, action),
     editedMessageId: editedMessageId(prevState.editedMessageId, action),
-    /*showMessages: showMessages(prevState.showMessages, action)
-    isLoading: isLoading(prevState.showMessages, action)*/
+    /*showMessages: showMessages(prevState.showMessages, action)*/
+    isLoading: isLoading(prevState.isLoading, action),
     loadingErrorMessage: loadMessagesRespone(prevState.loadingErrorMessage, action),
     crudErrorMessage: crudResponse(prevState.crudErrorMessage, action)
 });
