1e42e33f500b6b4c5673f9187ad272e333c9ee88
Jozef Pastucha <xpastuch@fi.muni.cz>
2018-12-01 23:39:37 +0100

    Resolved PR comments

--- src/utils/exportData.ts
+++ src/utils/exportData.ts
@@ -1,13 +1,9 @@
 import * as data from '../tests/mockData.json';
-import {IMessage} from '../Messages/model/IMessage';
-import * as Immutable from 'immutable';
-import {IUser} from '../Channels/models/IUser';
+
 
 // TODO: add types
-export const CHANNELS_DATA  =  (data as any).channels;
+
 
 export const USERS_DATA = (data as any).users;
 
-export const CHANNEL_MESSAGES_DATA: Immutable.List<IMessage> = (data as any).channelMessages;
 
-export const USER_DATA: IUser = (data as any).user;
