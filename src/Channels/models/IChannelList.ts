32b4bff5e9c88e272a029566b3b75382a74078b4
Jozef Pastucha <xpastuch@fi.muni.cz>
2018-12-02 01:43:43 +0100

    removed users from channelList

--- src/Channels/models/IChannelList.ts
+++ src/Channels/models/IChannelList.ts
@@ -1,14 +1,14 @@
 import * as Immutable from 'immutable';
 import { IChannel } from './IChannel';
 import {EditedChannels} from './EditedChannels';
-import {IUser} from './IUser';
+// import {IUser} from './IUser';
 import {IVisibilityFilter} from './IVisibilityFilter';
 
 export interface IChannelList {
     readonly channels: IChannels;
     readonly visibilityFilter: IVisibilityFilter;
     readonly editedChannelModal: EditedChannels;
-    readonly users: Immutable.List<IUser>;
+    // readonly users: Immutable.List<IUser>;
     readonly selectedChannelId: string;
     readonly showChannelList: boolean;
     readonly isLoading: boolean;
