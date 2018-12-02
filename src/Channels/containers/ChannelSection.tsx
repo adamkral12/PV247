38ec39711070b0278c5cc1fe39f30efa18b24e80
Jozef Pastucha <xpastuch@fi.muni.cz>
2018-12-02 03:53:40 +0100

    Trying to show only channels logged user is a member of

--- src/Channels/containers/ChannelSection.tsx
+++ src/Channels/containers/ChannelSection.tsx
@@ -6,14 +6,20 @@ import {ChannelSection, IChannelSectionStateProps} from '../components/ChannelSe
 import {IChannel} from '../models/IChannel';
 import {IVisibilityFilter} from '../models/IVisibilityFilter';
 
-const getVisibleChannelIds = createSelector<IState, IVisibilityFilter, Immutable.List<string>, Immutable.Map<string, IChannel>, Immutable.List<string>>(
+const getVisibleChannelIds = createSelector<IState, IVisibilityFilter, Immutable.List<string>, Immutable.Map<string, IChannel>, string, Immutable.List<string>>(
     [
         state => state.channelList.visibilityFilter,
         state => state.channelList.channels.allIds,
         state => state.channelList.channels.byId,
+        state => state.userApp.user.email
     ],
-    (visibilityFilter, allIds, byId) => {
+    (visibilityFilter, allIds, byId, userEmail) => {
+        console.log(userEmail);
+        // return allIds.filter((id: string) => byId.get(id).name.includes(visibilityFilter.text) && userEmail in byId.get(id).customData.members.keys()).toList();
+        // return allIds.filter((id: string) => byId.get(id).name.includes(visibilityFilter.text) && userEmail in byId.get(id).customData.members.has(userEmail)).toList();
+        // return allIds.filter((id: string) => byId.get(id).name.includes(visibilityFilter.text) && byId.get(id).customData.members.toSeq().has(userEmail)).toList();
         return allIds.filter((id: string) => byId.get(id).name.includes(visibilityFilter.text)).toList();
+
     });
 
 const mapStateToProps = (state: IState): IChannelSectionStateProps => {
