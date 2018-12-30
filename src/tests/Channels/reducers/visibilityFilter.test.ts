6673ed1e1c0b8197091c635a6d36a3c25c9fd5a7
JozefPastucha <pastucha25@gmail.com>
2018-12-30 17:38:48 +0100

    Update visibilityFilter.test.ts

--- src/tests/Channels/reducers/visibilityFilter.test.ts
+++ src/tests/Channels/reducers/visibilityFilter.test.ts
@@ -2,7 +2,7 @@ import {visibilityFilter} from '../../../Channels/reducers/visibilityFilter';
 import {CHANNEL_APP_SET_VISIBILITY_FILTER} from '../../../Channels/constants/actionTypes';
 
 describe('visibility filter reducer', () =>  {
-    it('setting vidibility filter should return text', () => {
+    it('setting visibility filter should return text', () => {
         expect(visibilityFilter(undefined, {
             type: CHANNEL_APP_SET_VISIBILITY_FILTER,
             payload: {
