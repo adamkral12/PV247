fcc8407f5fd293bcf82080f21048ad444f6d30c7
Jozef Pastucha <xpastuch@fi.muni.cz>
2019-01-04 18:10:47 +0100

    Removed unnecessary CSS class

--- src/Messages/components/CreateMessageRichTextEditor.tsx
+++ src/Messages/components/CreateMessageRichTextEditor.tsx
@@ -65,7 +65,7 @@ export class CreateMessageRichTextEditor extends React.PureComponent<IRichTextEd
 
   render() {
     return (
-        <div className="back">
+        <div>
             <Editor
                 editorState={this.state.editorState}
                 onEditorStateChange={this.onEditorStateChange}
