90bfdef8e3def899709aad0567cb57089cff0c87
Jozef Pastucha <xpastuch@fi.muni.cz>
2019-01-06 19:18:11 +0100

    Removed some comments

--- src/Screen/Screen.tsx
+++ src/Screen/Screen.tsx
@@ -5,10 +5,7 @@ export class Screen extends React.PureComponent {
   render() {
     return (
             <div>
-                <BodyWrapper
-                  // user={this.props.user}
-                  // messages={this.props.messages}
-                />
+                <BodyWrapper/>
             </div>
     );
   }
