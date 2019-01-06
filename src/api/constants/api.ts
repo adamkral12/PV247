604673287b22174623c523f9ec6b17f64a5dc586
Adam Král <adam.kral@kiwi.com>
2019-01-07 00:10:20 +0100

    reset app id

--- src/api/constants/api.ts
+++ src/api/constants/api.ts
@@ -1,4 +1,4 @@
-export const APP_ID = 'e73f8fff-04ef-422c-b5cd-3c9ded6f4971';
+export const APP_ID = 'fafdfd90-b276-45a0-8fc8-8385c2a87ba2';
 export const BASE_API_URL = 'https://pv247messaging.azurewebsites.net/api/v2/';
 
 export const HTTP_METHOD_GET = 'GET';
