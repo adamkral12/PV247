a8ad0b83e56b9b1c26a11d06a1ed5214cb59835d
Jozef Pastucha <xpastuch@fi.muni.cz>
2019-01-06 15:53:09 +0100

    Fixed typo

--- src/tests/Messages/reducers/loadResponse.test.ts
+++ src/tests/Messages/reducers/loadResponse.test.ts
@@ -1,6 +1,9 @@
 import {CHANNEL_APP_SELECT_CHANNEL_FAILURE, CHANNEL_APP_SELECT_CHANNEL_SUCCESS} from '../../../Channels/constants/actionTypes';
 import {loadResponse} from '../../../Messages/reducers/loadResponse';
-import {USER_APP_GET_USER_FAILURE, USER_APP_GET_USER_SUCCESS} from '../../../Users/constants/actionTypes';
+import {
+    USER_APP_GET_USERS_FAILURE,
+    USER_APP_GET_USERS_SUCCESS
+} from '../../../Users/constants/actionTypes';
 
 describe('load response reducer test', () => {
     it('select channel success should return undefined', () => {
@@ -18,13 +21,13 @@ describe('load response reducer test', () => {
 
     it('get users success should return undefined', () => {
         expect(loadResponse('Could not load', {
-            type: USER_APP_GET_USER_SUCCESS,
+            type: USER_APP_GET_USERS_SUCCESS,
         })).toEqual(undefined);
     });
 
     it('get users failure should return message', () => {
         expect(loadResponse(undefined, {
-            type: USER_APP_GET_USER_FAILURE,
+            type: USER_APP_GET_USERS_FAILURE,
             payload: { message: 'An error occurred.'}
         })).toEqual('An error occurred.');
     });
